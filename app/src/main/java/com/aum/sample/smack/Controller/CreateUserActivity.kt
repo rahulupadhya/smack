83dc8fb9ba305d8e9a7ac5acd9ce30bdac33143a
Rahul <rahulupadhyav@gmail.com>
2018-04-13 16:30:06 +0530

    Spinner, broadcast

--- app/src/main/java/com/aum/sample/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/aum/sample/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.aum.sample.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.aum.sample.smack.R
 import com.aum.sample.smack.Services.AuthService
 import com.aum.sample.smack.Services.UserDataService
+import com.aum.sample.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View) {
@@ -51,25 +56,55 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserBtnClicked(view: View) {
+        enableSpinner(true)
+
         val username = createUserNameTxt.text.toString()
         val email = createEmailTxt.text.toString()
         val password = createPasswordTxt.text.toString()
-        AuthService.registerUser(this, email, password) { registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) { loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, username, email, userAvatar, avatarColor) { createSuccess ->
-                            if (createSuccess) {
-                                print(UserDataService.name)
-                                print(UserDataService.avatarName)
-                                print(UserDataService.avatarColor)
-                                print(UserDataService.email)
-                                finish()
+
+        if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, email, password) { registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.loginUser(this, email, password) { loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, username, email, userAvatar, avatarColor) { createSuccess ->
+                                if (createSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Make sure username, email and password are filled in", Toast.LENGTH_SHORT).show()
+            enableSpinner(false)
         }
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
--- app/src/main/java/com/aum/sample/smack/Utilities/Constants.kt
+++ app/src/main/java/com/aum/sample/smack/Utilities/Constants.kt
@@ -1,6 +1,10 @@
 package com.aum.sample.smack.Utilities
 
 const val BASE_URL = "https://chattychatrahul.herokuapp.com/v1/"
+
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+// BROADCAST CONSTANTS
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
