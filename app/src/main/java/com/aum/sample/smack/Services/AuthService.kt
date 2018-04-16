3eed0982cc4ff2394b8104f7651a3dba74ecd08d
Rahul <rahulupadhyav@gmail.com>
2018-04-16 13:08:09 +0530

    Channels selection

--- app/src/main/java/com/aum/sample/smack/Services/AuthService.kt
+++ app/src/main/java/com/aum/sample/smack/Services/AuthService.kt
@@ -16,7 +16,7 @@ import org.json.JSONObject
 
 object AuthService {
 
-    fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
+    fun registerUser(email: String, password: String, complete: (Boolean) -> Unit) {
 
         val jsonBody = JSONObject();
         jsonBody.put("email", email)
@@ -41,7 +41,7 @@ object AuthService {
     }
 
 
-    fun loginUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
+    fun loginUser(email: String, password: String, complete: (Boolean) -> Unit) {
 
         val jsonBody = JSONObject();
         jsonBody.put("email", email)
@@ -74,7 +74,7 @@ object AuthService {
     }
 
 
-    fun createUser(context: Context, name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit) {
+    fun createUser(name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit) {
 
         val jsonBody = JSONObject();
         jsonBody.put("name", name)
